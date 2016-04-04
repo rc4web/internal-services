33481eb9dfef3ba8ab1dea28323e9a798a01d88b
Rey Neo <rey@live.jp>
2016-04-05 01:39:30 +0800

    Added claims, hide dining menu, change to OHS link

---
+++ claims/script.js
@@ -0,0 +1,22 @@
+// $(document).ready(function() {
+//
+//   var pdf = new jsPDF('p', 'pt', 'A4')
+//   var source = $('.claimform')[0];
+//
+//   var margins = {
+//      top: 25,
+//      bottom: 60,
+//      left: 20,
+//      width: 522
+//   };
+//
+//   // pdf.fromHTML(source, margins.left, margins.top, {}, download, margins);
+//   //
+//   // function download(dispose) {
+//   //     //pdf.save('Test.pdf');
+//   // }
+//   pdf.setFont("Arial", "Bold")
+//   pdf.setFontSize(9);
+//   pdf.text(36, 192, 'National University of Singapore');
+//   pdf.save('Test.pdf');
+// });
